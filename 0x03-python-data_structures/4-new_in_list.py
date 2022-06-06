d7f1c184c7380cd4cd601bd70f0fe193f9b5c1c6
musmax <musaojo1710@yahoo.com>
2022-06-06 09:13:21 -0700

    4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+def new_in_list(my_list, idx, element):
+    newList = []
+    length = len(my_list)
+    for i in range(length):
+        newList.append(my_list)
+    if newList(idx) = element:
+        return newList
+
